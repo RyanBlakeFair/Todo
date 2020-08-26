83829aa7850ac2e9dde0e40e042cb46d864778a6
RyanBlakeFair <ryanblakefair@gmail.com>
2020-08-27 09:28:37 +1200

    minify

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,32 +3,30 @@ import Typical from "react-typical";
 
 function Header() {
   return (
-    <div>
-      <p
-        style={{ background: "white", paddingLeft: "7rem" }}
-        className="font-bold text-3xl flex  text-left py-4 my-4"
-      >
-        <Typical
-          loop={Infinity}
-          wrapper="b"
-          steps={[
-            "Be organised.",
-            4000,
-            "Plans?",
-            4000,
-            "Get 💩 done.",
-            4000,
-            "Piece of 🎂",
-            4000,
-            "Finish it.",
-            4000,
-            "Start now.",
-            4000,
-            "Be productive.",
-            4000,
-          ]}
-        />
-      </p>
+    <div
+      style={{ background: "white", paddingLeft: "7rem" }}
+      className="text-4xl text-left py-4 my-4"
+    >
+      <Typical
+        loop={Infinity}
+        wrapper="b"
+        steps={[
+          "Be organised.",
+          4000,
+          "Plans?",
+          4000,
+          "Get 💩 done.",
+          4000,
+          "Piece of 🎂",
+          4000,
+          "Finish it.",
+          4000,
+          "Start now.",
+          4000,
+          "Be productive.",
+          4000,
+        ]}
+      />
     </div>
   );
 }
